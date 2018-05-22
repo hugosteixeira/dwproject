f36769a6cd566184743c6410c666509a7fce642f
Hugo Teixeira <hugosteixeira@hotmail.com>
2018-05-22 19:04:36 -0300

    Deu umas merdas mas deu bom

--- selenium_test.py
+++ selenium_test.py
@@ -1,33 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from datetime import date, timedelta, datetime
+import time
+driver = webdriver.Chrome()
 
-
-def login_twitter(driver, username, password):
- 
-    # open the web page in the browser:
-    driver.get("https://twitter.com/login")
- 
-    # find the boxes for username and password
-    username_field = driver.find_element_by_class_name("js-username-field")
-    password_field = driver.find_element_by_class_name("js-password-field")
- 
-    # enter your username:
-    username_field.send_keys(username)
-    driver.implicitly_wait(1)
- 
-    # enter your password:
-    password_field.send_keys(password)
-    driver.implicitly_wait(1)
- 
-    # click the "Log In" button:
-    driver.find_element_by_class_name("EdgeButtom--medium").click()
-
-driver = webdriver.Chrome(executable_path='C:\\Users\\joaov\\Desktop\\dwproject\\chromedriver_win32\\chromedriver.exe')
-#driver.get("https://twitter.com")
-#assert "Python" in driver.title
-login_twitter(driver, 'login', 'senha')#senha e nome do twitter (não ia por as minhas em um repositorio publico kkk)
-elementList = driver.find_elements_by_class_name("js-stream-tweet")
-idTweetList = [tweet.get_attribute("data-tweet-id") for tweet in elementList ]
-print(idTweetList, len(idTweetList))
-#assert "No results found." not in driver.page_source
-driver.close()
+query='lulalivre'
+dataInicial=date(2018,3,1)
+dataFinal=date(2018,5,1)
+print(str(dataInicial))
+driver.get("https://twitter.com/search?l=pt&q=%23" + query + "%20since%3A" + str(dataInicial) + "%20until%3A" + str(dataFinal) + "&src=typd&lang=pt")
+while True:
+    body = driver.find_element_by_tag_name('body')
+    body.send_keys(Keys.END)
+    time.sleep(3)
+    elementList = driver.find_elements_by_class_name("js-stream-tweet")
+    idTweetList = []
+    for tweet in elementList:
+        idTweetList.append(tweet.get_attribute("data-tweet-id"))
+    print(idTweetList, len(idTweetList))
