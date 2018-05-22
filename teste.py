3c04083c79ecb75ef1c3f5cfff6567ba3d6f9091
Hugo Teixeira <hugosteixeira@hotmail.com>
2018-05-22 19:03:38 -0300

    Auto stash before merge of "master" and "origin/master"

--- teste.py
+++ teste.py
@@ -1,15 +1,12 @@
 import tweepy
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
 
-
+driver = webdriver.Chrome(executable_path='C:\\Users\\hugos\\Desktop\\chromedriver_win32\\chromedriver.exe')
+cwd = os.getcwd()
 auth = tweepy.OAuthHandler('jkwDvQkT5Es6S24JiLq2FLxrb', 'ju5ogpsqo3cQLxtgTurMgq7cmWt8CN2H9lQ0F5wGGrmegcvAMp')
 auth.set_access_token('89299395-PpehItyb3bnxSI3TEbve9Y8uDZKKOgaYiQinCCrvg', 'Rh8FHQk0Vd66LCZJIf20DrFzFZfmBZqqPLaAN3hXCmT3n')
-
-
-
 api = tweepy.API(auth)
-
-public_tweets = api.home_timeline()
-
-
-for tweet in tweepy.Cursor(api.search,q="#pratodagalera",count=100,since="2010-04-03").items():
-    print (tweet.created_at, tweet.text+'\n')
+tweet = api.get_status('997192482843701249')
+print(tweet.text.encode('UTF-8').decode('UTF-8'))
