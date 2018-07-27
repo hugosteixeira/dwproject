fdd85c37ac5601f29cff1e3ab036214c95466073
Hugo Teixeira <hugosteixeira@hotmail.com>
2018-07-26 21:39:54 -0300

    Quase funcioanndo

--- format_place.py
+++ format_place.py
@@ -4,7 +4,7 @@ from flask import request, jsonify
 from flask.views import MethodView
 from sqlalchemy import inspect
 import googlemaps
-from utils import printError
+from utils import printError,limparTexto
     
 
 class FormatPlace :
@@ -16,42 +16,36 @@ class FormatPlace :
 
 
     def localizationAddress(self):
-        try:
-            print ("Minha localizacao e ", self.address_components)
-            geocode_result = self.gmaps.geocode(self.address_components,language='pt-br',region='BR')
-        
-            address_components = {}
-
-        
-            if len(geocode_result[0]['address_components'])==3:
-                address_components['city']= geocode_result[0]['address_components'][0]['long_name']
-                address_components['state']=geocode_result[0]['address_components'][1]['long_name']
-                address_components['country']=geocode_result[0]['address_components'][2]['long_name']
-            if len(geocode_result[0]['address_components'])==2:
-                address_components['city']= ''
-                address_components['state']=geocode_result[0]['address_components'][0]['long_name']
-                address_components['country']=geocode_result[0]['address_components'][1]['long_name']
-            if len(geocode_result[0]['address_components'])==1:
-                address_components['city']= ''
-                address_components['state']=''
-                address_components['country']=geocode_result[0]['address_components'][0]['long_name']
-            else:
-                address_components['city']= ''
-                address_components['state']=''
-                address_components['country']=''
-            print('teste',address_components)
-            return address_components
-
-        except:
-            address_components = {}
+        print ("Minha localizacao e ", self.address_components)
+        geocode_result = self.gmaps.geocode(self.address_components,language='pt-br')
+    
+        address_components = {}
+        print(geocode_result[0]['address_components'])
+        print(len(geocode_result[0]['address_components']))
+    
+        if len(geocode_result[0]['address_components'])>=3:
+            address_components['city']= geocode_result[0]['address_components'][-3]['long_name']
+            address_components['state']=geocode_result[0]['address_components'][-2]['long_name']
+            address_components['country']=geocode_result[0]['address_components'][-1]['long_name']
+        elif len(geocode_result[0]['address_components'])==2:
+            address_components['city']= ''
+            address_components['state']=geocode_result[0]['address_components'][0]['long_name']
+            address_components['country']=geocode_result[0]['address_components'][1]['long_name']
+        elif len(geocode_result[0]['address_components'])==1:
+            address_components['city']= ''
+            address_components['state']=''
+            address_components['country']=geocode_result[0]['address_components'][0]['long_name']
+        else:
             address_components['city']= ''
             address_components['state']=''
             address_components['country']=''
-            printError()
-            return address_components
+        print('teste',address_components)
+        return address_components
 
 
 
 
-
+texto = limparTexto('jabuataum, pe')
+formatPlace = FormatPlace(texto)
+print(formatPlace.localizationAddress())
  
